be59b4d3464d9d41a796595ac1f29d71f7bca0e0
marylychee <marylychee@pm.me>
2025-05-04 15:50:18 +1000

    created react form and ability to input csv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './styles.css';
 
 function App() {
+  const [file, setFile] = useState();
+  
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  
   return (
     <div className="App">
-      <form>
-          <input type={"file"} accept={".csv"} />
-          <button>Import CSV</button>       
+      <h1>CSV File Uploader</h1>
+      <form className='Form-style'>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
       </form>
     </div>
   );
